752c64c6199cceded0646cd2ab90e95dea848713
Rahul Tyagi <tyagi8285@gmail.com>
2021-09-21 21:34:31 +0530

    student class create

--- CSharpProject/Program.cs
+++ CSharpProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpProject.programming;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace CSharpProject
     {
         static void Main(string[] args)
         {
+            Student obj = new Student("Amit Kumar",25,350,"BCA");
+            obj.GetStudent();
+            Console.ReadKey();   
         }
     }
 }
